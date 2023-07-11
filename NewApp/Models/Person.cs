b38711d66a3ed01afc5f3ad6e6e58187ceae88c7
hattiu <hattiu210817@gmail.com>
2023-07-10 20:28:14 -0700

    BaiThucHanh1007

---
+++ NewApp/Models/Person.cs
@@ -0,0 +1,53 @@
+namespace NewApp.Models
+{
+    public class Person 
+    {
+        //khai bao cac thuoc tinh
+        public string FullName {get;set;}
+        public string Address {get;set;}
+        public string Age {get;set;}
+        
+        //khai bao cac phuong thuc
+        public void NhapDuLieu()
+        {
+            System.Console.Write("Full Name = ");
+            FullName = Console.ReadLine();
+
+            System.Console.Write("Address = ");
+            Address = Console.ReadLine();
+
+            System.Console.Write("Age = ");
+            Age = Console.ReadLine();
+            
+        }
+
+        public int GetYearOfBirth (int age)
+        {
+            int yearOfBirth = 2023 - age;
+            return yearOfBirth;
+        }
+        
+
+        public void HienThi ()
+        {
+            System.Console.WriteLine("{0} - {1} - {2} tuoi", FullName, Address, Age);
+            
+        }
+
+        public void HienThi (string ten, int tuoi)
+        {
+            System.Console.WriteLine("Sinh vien {0} - {1} tuoi", ten, tuoi);
+            
+        }
+        
+        public void HienThi(string ten, string diaChi)
+        {
+            System.Console.WriteLine("Sinh vien {0} - {1} tuoi", ten, diaChi);
+            
+        }
+        
+
+    }
+
+}
+
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,38 +1,17 @@
-﻿public class Program
+﻿using  NewApp.Models;
+
+public class Program 
 {
-    
-    private static void Main(string [] args)
+    private static void Main(string[] args)
     {
-        //Khai bao bie kieu string va gan gia tri = Nguyen Van A
-       
-        string hoTen = "Nguyen Van A";
-        //Khai bao bien kieu in vaf gans gia tri  = 123456
-        int maSinhVien = 123456;
-        Console.WriteLine("Sinh vien: {0} - Ma sinh vien: {1} ",hoTen,maSinhVien);
+        //khoi tao hai doi tuong tu class Person
+        Person ps = new Person();
         
-        int a,b,c,d,e;//khai bao 5 bien so a,b,c,d,e kieu int
-        //thong bao cho nguoi dung nhap vao a
-        System.Console.Write("Toan: ");
-        //doc du lieu nhap vao tu ban phism chuyen sang kieu int va gan cho a
-        a = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.Write("Van: ");
-        b = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.Write("Anh: ");
-        c = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.Write("Li: ");
-        d = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.Write("Hoa: ");
-        e = Convert.ToInt32(Console.ReadLine());
-
-        //in ra man hinh diem trung binh
-        System.Console.WriteLine("Tong diem: {0} + {1} + {2} + {3} + {4} = {5} ",a,b,c,d,e,a+b+c+d+e);
-       
-        
-       
+        //gan gia tri ch thuoc tinh doi tuong ps1
+        string str = "Nguyen Van A";
+        int a  = 22;
+        Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
     }
-}
         
+
+}
